e1ee882d2795e11e3ae429f113f5c8549ccdb56c
Rajat Roy <rroy1@illumina.com>
2019-10-21 23:47:00 -0700

    adding optional parameters

--- src/com/ola/Add.java
+++ src/com/ola/Add.java
@@ -4,6 +4,7 @@ import static java.lang.Integer.valueOf;
 
 
 public class Add {
+    private static String commandSyntex = "add  -i {string} -g {string} -l {integer} [-a {string}] [-t {string} -y {integer}]";
     public static void Run(String[] args){
         Options options = new Options();
 
@@ -11,20 +12,26 @@ public class Add {
         bookId.setRequired(true);
         options.addOption(bookId);
 
-        Option genre = new Option("g", "gen", true, "book genre (FIC, NFI, SOC, SCI)");
+        Option genre = new Option("g", "genre", true, "book genre (FIC, NFI, SOC, SCI)");
         bookId.setRequired(true);
         options.addOption(genre);
 
-        Option readingLevel = new Option("l", "lev", true, "Reading level (1 to 5)");
+        Option readingLevel = new Option("l", "level", true, "Reading level (1 to 5)");
         bookId.setRequired(true);
         options.addOption(readingLevel);
+
+        Option author = new Option("a", "author", true, "Author Name (optional)");
+        options.addOption(author);
+
+        Option title = new Option("t", "title", true, "Title of the book (optional)");
+        options.addOption(title);
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd;
 
         if(args.length==1) {
-            formatter.printHelp("add  -i {isbn number} -g {genre} -l {reading level}", options);
+            formatter.printHelp(commandSyntex, options);
             return;
         }
 
@@ -34,15 +41,15 @@ public class Add {
         }
         catch (ParseException e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("add  -i {isbn number} -g {genre} -l {reading level}", options);
+            formatter.printHelp(commandSyntex, options);
 
         }
     }
 
     private static void AddNewBook(CommandLine cmd) {
         String isbn = cmd.getOptionValue("isbn");
-        String genre =  cmd.getOptionValue("gen");
-        Integer readingLevel = valueOf(cmd.getOptionValue("lev"));
+        String genre =  cmd.getOptionValue("genre");
+        Integer readingLevel = valueOf(cmd.getOptionValue("level"));
         System.out.printf("Adding new book: isbn: %s, genre: %s, reading level: %d \n", isbn, genre, readingLevel);
     }
 }
