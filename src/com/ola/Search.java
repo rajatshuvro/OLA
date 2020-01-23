d4d5d9668271351e8eb19da7b4bf7394b5ba6a6e
Rajat Roy <rroy1@illumina.com>
2020-01-22 17:23:02 -0800

    making search prettier

--- src/com/ola/Search.java
+++ src/com/ola/Search.java
@@ -17,26 +17,31 @@ public class Search {
 
         var bookDb = dataProvider.BookDb;
         var bookSearchIndex = bookDb.GetSearchIndex();
+        System.out.println("----------------------Books----------------------");
         for (var id: bookSearchIndex.Search(queryText, 10)) {
-            System.out.println(FlatObjectParser.RecordSeparator);
             System.out.println(bookDb.GetBook(id).toString());
+            System.out.println(FlatObjectParser.RecordSeparator);
         }
 
         var userDb = dataProvider.UserDb;
         var userSearchIndex = userDb.GetSearchIndex();
+        System.out.println('\n');
+        System.out.println("----------------------Users----------------------");
         for(var id: userSearchIndex.Search(queryText, 10)){
-            System.out.println(FlatObjectParser.RecordSeparator);
             System.out.println(userDb.GetUser(id).toString());
+            System.out.println(FlatObjectParser.RecordSeparator);
         }
 
         var transactionDb = dataProvider.TransactionDb;
         var transactionsSearchIndex = transactionDb.GetSearchIndex();
+        System.out.println('\n');
+        System.out.println("----------------------Transactions--------------------");
         for(var index: transactionsSearchIndex.Search(queryText, 10)){
-            System.out.println(FlatObjectParser.RecordSeparator);
             System.out.println(transactionDb.Get(index).toString());
+            System.out.println(FlatObjectParser.RecordSeparator);
         }
-        System.out.println(FlatObjectParser.RecordSeparator);
-        System.out.println("-------------------End of search results----------------");
+        System.out.println('\n');
+        System.out.println("=========================End of search results=====================");
     }
 
     private static String ConstructQuery(String[] args) {
